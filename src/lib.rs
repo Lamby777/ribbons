f80d43b757f011d49f01d00f7d2724c75080d89f
Cherry <cherry@sparklet.org>
2024-01-18 20:51:53 -0500

    use `!` type + renamed some stuff

--- src/lib.rs
+++ src/lib.rs
@@ -3,41 +3,35 @@
 //! unwrapping either `Option` or `Result` with a formatted string.
 //!
 
-// #![feature]
+#![feature(never_type)]
 
 /// like `expect` but formatted string. works for both `Option` and `Result`
 #[macro_export]
 macro_rules! unwrap_fmt {
     ($expr:expr, $($rest:tt),*) => {
-        FmtExpect::expect_fmt($expr, || panic!($($rest),*))
+        $crate::FmtExpect::expect_fmt($expr, || panic!($($rest),*))
     };
 }
 
-/// Use the `unwrap_that_mf!` macro instead.
+/// Use the `unwrap_fmt!` macro instead.
 ///
 /// The only reason this exists is because `Option` and `Result`
 /// have a different number of arguments for `unwrap_or_else` so
 /// it's not possible to use the same macro for both.
 pub trait FmtExpect<T> {
-    fn expect_fmt(self, panic: impl FnOnce()) -> T;
+    fn expect_fmt(self, panic: impl FnOnce() -> !) -> T;
 }
 
 impl<T> FmtExpect<T> for Option<T> {
-    fn expect_fmt(self, panic: impl FnOnce()) -> T {
-        self.unwrap_or_else(|| {
-            panic();
-            unreachable!()
-        })
+    fn expect_fmt(self, panic: impl FnOnce() -> !) -> T {
+        self.unwrap_or_else(|| panic())
     }
 }
 
 impl<T, E> FmtExpect<T> for Result<T, E> {
-    fn expect_fmt(self, panic: impl FnOnce()) -> T {
+    fn expect_fmt(self, panic: impl FnOnce() -> !) -> T {
         // ignore the damn argument!!!
-        self.unwrap_or_else(|_| {
-            panic();
-            unreachable!()
-        })
+        self.unwrap_or_else(|_| panic())
     }
 }
 
@@ -46,7 +40,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn unwrap_that_some() {
+    fn unwrap_some() {
         let opt = Some(1);
         let n = unwrap_fmt!(opt, "failed, beep boop.");
         assert_eq!(n, 1);
@@ -54,13 +48,13 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "expected 1, got None")]
-    fn unwrap_that_none() {
+    fn unwrap_none() {
         let opt: Option<i32> = None;
         unwrap_fmt!(opt, "expected {}, got {:?}", 1, opt);
     }
 
     #[test]
-    fn unwrap_that_ok() {
+    fn unwrap_ok() {
         let res: Result<i32, ()> = Ok(1);
         let n = unwrap_fmt!(res, "failed, beep boop.");
         assert_eq!(n, 1);
@@ -68,7 +62,7 @@ mod tests {
 
     #[test]
     #[should_panic(expected = "expected 1, got Err(())")]
-    fn unwrap_that_err() {
+    fn unwrap_err() {
         let res: Result<i32, ()> = Err(());
         unwrap_fmt!(res, "expected {}, got {:?}", 1, res);
     }
